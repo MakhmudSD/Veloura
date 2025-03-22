1f20890d90633eceded27efc138e61a3542e4c08
MakhmudSD <makhwork15@gmail.com>
2025-03-22 15:49:44 +0900

    feat: Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+// import router from "./router";
+// import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "../src/libs/config";
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// app.use("/", router);
+// app.use("/admin", routerAdmin);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config()
+
+import mongoose, { mongo } from "mongoose";
+import app from "./app"
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connected successfully");
+    const PORT = process.env.PORT;
+    app.listen(PORT, function () {
+      console.info(`The app is running successfully on ${PORT}`);
+      console.info(`Admin Project is on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch((err) => {
+    console.info("Error on MongoDB connection", err);
+  });
