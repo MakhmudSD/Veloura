501356ae79f0b3ae1ee364640a1ac1c383f7c115
MakhmudSD <makhwork15@gmail.com>
2025-03-30 12:05:15 +0900

    feat: integrating service model of Service Controller

---
+++ src/models/Service.Service.ts
@@ -0,0 +1,12 @@
+import ServiceModel from "../schema/Service.model";
+
+class ServiceService {
+    private readonly serviceModel;
+
+    constructor() {
+        this.serviceModel = ServiceModel
+    }
+}
+
+export default ServiceService
+
