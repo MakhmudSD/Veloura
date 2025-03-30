4fba94a086a19d2e5d1f4ea90db587084fef896e
MakhmudSD <makhwork15@gmail.com>
2025-03-30 12:04:31 +0900

    feat: service enum developed based on ER

---
+++ src/libs/enums/services.enum.ts
@@ -0,0 +1,28 @@
+export enum ServiceSize {
+    BASIC = "BASIC",
+    STANDARD = "STANDARD",
+    PREMIUM = "PREMIUM",
+    PACKAGE = "PACKAGE",
+  }
+  
+  export enum ServiceDuration {
+    SHORT = 0.5, // 30 minutes
+    STANDARD = 1, // 1 hour
+    EXTENDED = 1.5, // 1.5 hours
+    VIP = 2, // 2 hours
+  }
+  
+  export enum ServiceStatus {
+    AVAILABLE = "AVAILABLE",
+    IN_PROGRESS = "IN_PROGRESS",
+    UNAVAILABLE = "UNAVAILABLE",
+  }
+  
+  export enum ServiceCategory {
+    HAIRCUT = "HAIRCUT",
+    BEARD_TRIM = "BEARD_TRIM",
+    COLORING = "COLORING",
+    FACIAL = "FACIAL",
+    OTHERS = "OTHERS",
+  }
+  
