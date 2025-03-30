e74343f54872528dae11d1d3cc6ad1d97206f048
MakhmudSD <makhwork15@gmail.com>
2025-03-30 12:03:46 +0900

    fix: renaming controllers

---
+++ src/controllers/service.controller.ts
@@ -0,0 +1,45 @@
+import Errors from "../libs/Errors";
+import { T } from "../libs/types/common";
+import { Request, Response } from "express";
+
+const serviceController: T = {};
+
+serviceController.getAllProducts = async (req: Request, res: Response) => {
+  {
+    try {
+      console.log("getAllProducts");
+      res.render("products");
+    } catch (err) {
+      console.log("ERROR on getAllProducts");
+      if (err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  }
+};
+
+serviceController.createNewProduct = async (req: Request, res: Response) => {
+  {
+    try {
+      console.log("createNewProduct");
+      res.send("File Uploaded Successfully");
+    } catch (err) {
+      console.log("ERROR on createNewProduct");
+      if (err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  }
+};
+
+serviceController.updateChosenProduct = async (req: Request, res: Response) => {
+  {
+    try {
+      console.log("updateChosenProduct");
+    } catch (err) {
+      console.log("ERROR on updateChosenProduct");
+      if (err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  }
+};
+
+export default serviceController;
