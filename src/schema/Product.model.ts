1ab2a69e39d238684e647efe7ecbcec9ce2c9f16
MakhmudSD <makhwork15@gmail.com>
2025-03-30 12:50:47 +0900

    fix: product schema model modified

---
+++ src/schema/Product.model.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductDuration,
+  ProductStatus,
+  ProductCategory,
+  ProductSize,
+} from "../libs/enums/products.enum";
+
+// Build type: Schema First vs Code First
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.IN_PROGRESS,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      default: ProductSize.STANDARD,
+    },
+
+    productCategory: {
+      type: String,
+      enum: ProductCategory,
+      default: ProductCategory.HAIRCUT,
+    },
+
+    productDuration: {
+      type: String,
+      enum: ProductDuration,
+      default: ProductDuration.STANDARD,
+    },
+
+    productDesc: {
+      type: String,
+      required: true,
+    },
+
+    productImages: {
+      type: { String },
+      default: [],
+    },
+
+    productViews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } // updatedAt, createdAt info
+);
+
+productSchema.index(
+  {
+    Productategory: 1,
+    ProductSize: 1,
+    ProductDuration: 1,
+  },
+  { unique: true }
+);
+
+export default mongoose.model("Product", productSchema);
