fd39251b0fac60232df45f81f98957b2a900cf1c
MakhmudSD <makhwork15@gmail.com>
2025-03-30 12:48:54 +0900

    fix: renamed back

---
+++ src/libs/enums/products.enum.ts
@@ -0,0 +1,28 @@
+export enum ProductSize {
+    BASIC = "BASIC",
+    STANDARD = "STANDARD",
+    PREMIUM = "PREMIUM",
+    PACKAGE = "PACKAGE",
+  }
+  
+  export enum ProductDuration {
+    SHORT = 0.5, // 30 minutes
+    STANDARD = 1, // 1 hour
+    EXTENDED = 1.5, // 1.5 hours
+    VIP = 2, // 2 hours
+  }
+  
+  export enum ProductStatus {
+    AVAILABLE = "AVAILABLE",
+    IN_PROGRESS = "IN_PROGRESS",
+    UNAVAILABLE = "UNAVAILABLE",
+  }
+  
+  export enum ProductCategory {
+    HAIRCUT = "HAIRCUT",
+    BEARD_TRIM = "BEARD_TRIM",
+    COLORING = "COLORING",
+    FACIAL = "FACIAL",
+    OTHERS = "OTHERS",
+  }
+  
