edd5b11c11f6eea00bda6a893cff0be8c5128b4b
MakhmudSD <makhwork15@gmail.com>
2025-04-02 15:32:06 +0900

    feat: createNewProduct product service model integrated

--- src/models/Product.Service..ts
+++ src/models/Product.Service..ts
@@ -1,11 +1,29 @@
+import { ProductInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
+import { Product } from "../libs/types/product";
+import Errors, { Message } from "../libs/Errors";
+import { HttpCode } from "../libs/Errors";
 
 class ProductService {
-    private readonly productModel;
+  private readonly productModel;
 
-    constructor() {
-        this.productModel = ProductModel
+  constructor() {
+    this.productModel = ProductModel;
+  }
+
+  /** SPA */
+
+  /** SSR */
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      const result = await this.productModel.create(input);
+      return result as unknown as Product;
+    } catch (err) {
+      console.log("ERROR, model: createNewProduct", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.NO_MEMBER_NICK);
     }
+  }
 }
 
 export default ProductService
