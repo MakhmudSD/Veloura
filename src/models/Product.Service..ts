9caf1c23f330f05c50dfc65eb92de0f6bd9577da
MakhmudSD <makhwork15@gmail.com>
2025-04-02 23:37:57 +0900

    fix: product service model adjusted to product controller APIs

--- src/models/Product.Service..ts
+++ src/models/Product.Service..ts
@@ -1,8 +1,9 @@
-import { ProductInput } from "../libs/types/product";
+import { ProductInput, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { Product } from "../libs/types/product";
 import Errors, { Message } from "../libs/Errors";
 import { HttpCode } from "../libs/Errors";
+import { shapeIntoMongooseObjectId } from "../libs/config";
 
 class ProductService {
   private readonly productModel;
@@ -15,6 +16,12 @@ class ProductService {
 
   /** SSR */
 
+  public async getAllProducts(): Promise<Product> {
+    const result = await this.productModel.find().exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result as unknown as Product;
+  }
+
   public async createNewProduct(input: ProductInput): Promise<Product> {
     try {
       const result = await this.productModel.create(input);
@@ -24,7 +31,18 @@ class ProductService {
       throw new Errors(HttpCode.BAD_REQUEST, Message.NO_MEMBER_NICK);
     }
   }
+
+  public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput
+  ): Promise<Product> {
+    id = shapeIntoMongooseObjectId(id);
+    const result = await this.productModel
+      .findOneAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.UPDATE_FAILED);
+    return result as unknown as Product;
+  }
 }
 
-export default ProductService
-
+export default ProductService;
