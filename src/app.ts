a922c716d349649b881a163122a0965efd2fab77
MakhmudSD <makhwork15@gmail.com>
2025-04-04 14:34:13 +0900

    fix: comments added into parts of express

--- src/app.ts
+++ src/app.ts
@@ -14,12 +14,15 @@ const store = new MongDbStore({
   collection: "sessions",
 });
 
+// 1 Entrance
+
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
+// 2 Session
 app.use(
   session({
     secret: String(process.env.SESSION_SECRET),
@@ -39,9 +42,11 @@ app.use(function (req, res, next) {
   next();
 });
 
+// 3 Views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// 4 Routing
 app.use("/", router);
 app.use("/admin", routerAdmin);
 
