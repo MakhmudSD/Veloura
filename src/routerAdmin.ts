8b8eabbac1d8bc0454419cc749e199ced5ab6cd4
MakhmudSD <makhwork15@gmail.com>
2025-04-05 17:36:19 +0900

    feat: user APIs built

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const routerAdmin = express.Router();
+
 import barberController from "../src/controllers/barber.controller";
 import productController from "./controllers/product.controller";
 import makeUploader from "./libs/utils/uploader";
@@ -7,21 +8,23 @@ import makeUploader from "./libs/utils/uploader";
 // /** BarberMaster */
 
 routerAdmin.get("/", barberController.goHome);
+
 routerAdmin
   .get("/signup", barberController.getSignup)
   .post(
     "/signup",
     makeUploader("members").single("memberImage"),
-    barberController.signup
+    barberController.signup // <-- fixed name
   );
 
 routerAdmin
   .get("/login", barberController.getLogin)
-  .post("/login", barberController.login);
+  .post("/login", barberController.login); // <-- fixed name
 
 routerAdmin.get("/logout", barberController.logout);
 routerAdmin.get("/check-me", barberController.checkAuthSession);
 
+// Products
 routerAdmin
   .get(
     "/product/all",
@@ -39,5 +42,17 @@ routerAdmin
     barberController.verifyBarber,
     productController.updateChosenProduct
   );
+
+// Users
+routerAdmin.get(
+  "/user/all",
+  barberController.verifyBarber,
+  barberController.getUsers
+);
+routerAdmin.post(
+  "/user/edit",
+  barberController.verifyBarber,
+  barberController.updateChosenUser
+);
 
 export default routerAdmin;
