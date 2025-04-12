487aa7127c1d714cacd17ec82667b7f8e72c5be8
MakhmudSD <makhwork15@gmail.com>
2025-04-12 17:57:59 +0900

    fix: updates added

--- src/libs/enums/products.enum.ts
+++ src/libs/enums/products.enum.ts
@@ -1,36 +1,20 @@
 export enum ProductStyle {
-    BASIC = "BASIC",
-    STANDARD = "STANDARD",
-    PREMIUM = "PREMIUM",
-    PACKAGE = "PACKAGE",
-  }
-  
-  
-  export enum ProductStatus {
-    AVAILABLE = "AVAILABLE",
-    PROCESS = "PROCESS",
-    UNAVAILABLE = "UNAVAILABLE",
-  }
+  BASIC = "BASIC",
+  STANDARD = "STANDARD",
+  PREMIUM = "PREMIUM",
+  PACKAGE = "PACKAGE",
+}
 
-  // export enum ProductDuration {
-  //   HALF_HOUR = 30,  // 30 minutes
-  //   ONE_HOUR = 60,   // 1 hour
-  //   TWO_HOURS = 120, // 2 hours
-  //   VIP_TARIFF = 180 // 3 hours for VIP services
-  // }
-  
-  export enum ProductCategory {
-    HAIRCUT = "HAIRCUT",
-    BEARD_TRIM = "BEARD_TRIM",
-    COLORING = "COLORING",
-    FACIAL = "FACIAL",
-    OTHERS = "OTHERS",
-  }
-  
-  export enum ProductPackageSize {
-    SMALL = 'SMALL',
-    MEDIUM = 'MEDIUM',
-    LARGE = 'LARGE',
-    XL = 'EXTRA LARGE',
-    TRAVEL = 'TRAVEL SIZE',
-  }
+export enum ProductStatus {
+  AVAILABLE = "AVAILABLE",
+  PROCESS = "PROCESS",
+  UNAVAILABLE = "UNAVAILABLE",
+}
+
+export enum ProductCategory {
+  HAIRCUT = "HAIRCUT",
+  BEARD_TRIM = "BEARD_TRIM",
+  COLORING = "COLORING",
+  FACIAL = "FACIAL",
+  OTHERS = "OTHERS",
+}
