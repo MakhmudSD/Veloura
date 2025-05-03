9c2fdf6f190c1fdb56b1ca177a2c4e2c781f9766
MakhmudSD <makhwork15@gmail.com>
2025-05-04 07:17:52 +0900

    feat: create getProducts service model

--- src/models/Product.Service..ts
+++ src/models/Product.Service..ts
@@ -1,9 +1,11 @@
-import { ProductInput, ProductUpdateInput } from "../libs/types/product";
+import { ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { Product } from "../libs/types/product";
 import Errors, { Message } from "../libs/Errors";
 import { HttpCode } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { ProductStatus } from "../libs/enums/products.enum";
+import { T } from "../libs/types/common";
 
 class ProductService {
   private readonly productModel;
@@ -13,6 +15,32 @@ class ProductService {
   }
 
   /** SPA */
+
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    console.log("inquiry:", inquiry);
+
+    const match: T = { productStatus: ProductStatus.AVAILABLE };
+
+    if (inquiry.productCategory)
+      match.productCategory = inquiry.productCategory;
+    if (inquiry.search)
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 }
+        : { [inquiry.order]: -1 };
+
+    const result = await this.productModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page * 1 - 1) * inquiry.limit },
+        { $limit: inquiry.limit * 1 },
+      ])
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
 
   /** SSR */
 
@@ -36,7 +64,6 @@ class ProductService {
     id: string,
     input: ProductUpdateInput
   ): Promise<Product> {
-
     id = shapeIntoMongooseObjectId(id);
     const result = await this.productModel
       .findOneAndUpdate({ _id: id }, input, { new: true })
