63222e952b68c8673223cae0a5f8d803c9f3f473
MakhmudSD <makhwork15@gmail.com>
2025-05-07 15:52:28 +0900

    feat: create getProduct service model

--- src/models/Product.Service..ts
+++ src/models/Product.Service..ts
@@ -1,4 +1,8 @@
-import { ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
+import {
+  ProductInput,
+  ProductInquiry,
+  ProductUpdateInput,
+} from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { Product } from "../libs/types/product";
 import Errors, { Message } from "../libs/Errors";
@@ -6,12 +10,18 @@ import { HttpCode } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/products.enum";
 import { T } from "../libs/types/common";
+import { ObjectId } from "mongoose";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
+import ViewService from "./View.service";
 
 class ProductService {
   private readonly productModel;
+  public viewService;
 
   constructor() {
     this.productModel = ProductModel;
+    this.viewService = new ViewService();
   }
 
   /** SPA */
@@ -40,6 +50,42 @@ class ProductService {
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
+  }
+
+  public async getProduct(
+    memberId: ObjectId | null,
+    id: string
+  ): Promise<Product> {
+    const productId = shapeIntoMongooseObjectId(id);
+    let result = await this.productModel
+      .findOne({ _id: productId, productStatus: ProductStatus.AVAILABLE })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    if (memberId) {
+      const input: ViewInput = {
+        memberId: memberId,
+        viewRefId: productId,
+        viewGroup: ViewGroup.PRODUCT,
+      };
+
+      const existView = await this.viewService.checkViewExistence(input);
+
+      console.log("existView", existView);
+
+      if (!existView) {
+        await this.viewService.insertMemberView(input);
+
+        result = await this.productModel
+          .findByIdAndUpdate(
+            productId,
+            { $inc: { productViews: +1 } },
+            { new: true }
+          )
+          .exec();
+      }
+    }
+    return result as unknown as Product;
   }
 
   /** SSR */
