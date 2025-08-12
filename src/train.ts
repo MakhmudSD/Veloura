b25a77d112850d6c661849713ea6da9d6df12dc4
MakhmudSD <makhwork15@gmail.com>
2025-08-13 01:21:58 +0900

    fix: frontend navbar fixed

---
+++ src/train.ts
@@ -0,0 +1,13 @@
+/* PM2 COMMANDS
+
+pm2 list
+pm2 start dist/server.js --name VELOURA-PERFUME
+pm2 start "npm run start:prod" --name VELOURA-PERFUME
+pm2 stop id
+pm2 delete id
+pm2 start id
+pm2 restart id
+pm2 monit 
+pm2 kill
+
+*/
